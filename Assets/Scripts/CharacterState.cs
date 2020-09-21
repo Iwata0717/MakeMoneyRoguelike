5a94909c2d01cc447828df340db9387d72e08773
Iwata0717 <zgkhbi0717@outlook.jp>
2020-09-22 07:25:54 +0900

    Initial commit

---
+++ Assets/Scripts/CharacterState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterState : MonoBehaviour
+{
+	private int _hp;
+	private int _attack;
+	private int _defense;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+	private EnemyController[] _enemys = new EnemyController[10];
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		Spawn(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	//
+	public void Spawn(int spawnValue)
+	{
+
+	}
+}
